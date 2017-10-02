52fcb23b6c6b8a342257b25a218b503cdf8fe087
Ben Blackman <bblkmn5@gmail.com>
2017-10-02 22:15:34 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  arrays = []
+  if block_given?
+    arrays.each do |v|
+      yield v
+  end
+else
+  arrays
+end
+end
 
-
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) do |language|
+  language.upcase
+end
